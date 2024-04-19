3e0721dc819a2c96e259a381a82b731595e5b3ab
Anton Balashov <balashov.anton.1983@gmail.com>
2024-04-19 20:16:25 +0300

    Update Control Flow Type analysis example

--- app.ts
+++ app.ts
@@ -6,8 +6,14 @@ function GetReview(title: string): string | number {
   }
 }
 
-const movieTitle: string = 'A New Hope';
+const movieTitle: string = 'The Empire Strikes Back';
 
 let movieReview: string | number = GetReview(movieTitle);
 
 console.log(`Movie title: ${movieTitle}`);
+
+if (typeof movieReview === 'string') {
+  console.log(`Review: ${movieReview}`);
+} else {
+  console.log(`Rating: ${movieReview}/10`);
+}
