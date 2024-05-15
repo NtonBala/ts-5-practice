34246054d9115b338ccf7c5a8792ff3df84b2c45
Anton Balashov <balashov.anton.1983@gmail.com>
2024-05-15 20:45:35 +0300

    Extract Control Flow Type Analysis example

--- app.ts
+++ app.ts
@@ -1,19 +1,2 @@
-function GetReview(title: string): string | number {
-  if (title === 'A New Hope') {
-    return 'An instant classic!';
-  } else {
-    return Math.floor(Math.random() * 10);
-  }
-}
-
-const movieTitle: string = 'The Empire Strikes Back';
-
-let movieReview: string | number = GetReview(movieTitle);
-
-console.log(`Movie title: ${movieTitle}`);
-
-if (typeof movieReview === 'string') {
-  console.log(`Review: ${movieReview}`);
-} else {
-  console.log(`Rating: ${movieReview}/10`);
-}
+let greeting: string = 'Hello TS!';
+console.log(greeting);
