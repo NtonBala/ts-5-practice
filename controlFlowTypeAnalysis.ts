4dcbfbd233fb34af693fb1970a0335f5c5a104ef
Anton Balashov <balashov.anton.1983@gmail.com>
2024-05-15 23:47:39 +0300

    Comment Control Flow Type Analysis case

--- controlFlowTypeAnalysis.ts
+++ controlFlowTypeAnalysis.ts
@@ -12,8 +12,11 @@ let movieReview: string | number = GetReview(movieTitle);
 
 console.log(`Movie title: ${movieTitle}`);
 
+// ! Here movieReview is union type: string | number
 if (typeof movieReview === 'string') {
+  // ! Here movieReview is of type string
   console.log(`Review: ${movieReview}`);
 } else {
+  // ! Here movieReview is of type number
   console.log(`Rating: ${movieReview}/10`);
 }
