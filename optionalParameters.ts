dff1711107a2c556ed3252a1f6e9831d5f591b7a
Anton Balashov <balashov.anton.1983@gmail.com>
2024-05-16 23:51:56 +0300

    Comment optional parameters processing case

--- optionalParameters.ts
+++ optionalParameters.ts
@@ -1,6 +1,7 @@
 function PrintMovieInfo(title: string, yearReleased?: number) {
   console.log(`Title: ${title}`);
 
+  // ! When working with optional parameters, a developer has first to check if it contains an actual value
   if (yearReleased) {
     console.log(`Year Released: ${yearReleased}`);
   }
